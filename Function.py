29460eedfe416e9f6ef477e86776d5b72832bf8b
Nur Azizah Novitami <azizahnovitami@gmail.com>
2019-07-22 00:27:02 +0800

    Add function

---
+++ Function.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import math
+import random
+
+
+###################################################################################################
+# use Pythagorean theorem to calculate distance between two chars
+def distanceBetweenChars(firstChar, secondChar):
+    intX = abs(firstChar.intCenterX - secondChar.intCenterX)
+    intY = abs(firstChar.intCenterY - secondChar.intCenterY)
+
+    return math.sqrt((intX ** 2) + (intY ** 2))
+# end function
+
+###################################################################################################
+# use basic trigonometry (SOH CAH TOA) to calculate angle between chars
+def angleBetweenChars(firstChar, secondChar):
+    fltAdj = float(abs(firstChar.intCenterX - secondChar.intCenterX))
+    fltOpp = float(abs(firstChar.intCenterY - secondChar.intCenterY))
+
+    if fltAdj != 0.0:                           # check to make sure we do not divide by zero if the center X positions are equal, float division by zero will cause a crash in Python
+        fltAngleInRad = math.atan(fltOpp / fltAdj)      # if adjacent is not zero, calculate angle
+    else:
+        fltAngleInRad = 1.5708                          # if adjacent is zero, use this as the angle, this is to be consistent with the C++ version of this program
+    # end if
+
+    fltAngleInDeg = fltAngleInRad * (180.0 / math.pi)       # calculate angle in degrees
+
+    return fltAngleInDeg
+# end function
